88148f9a858d1a40b82ae1108a7e3872d610e65d
Vsnikhilmaheswar <84986865+Vsnikhilmaheswar@users.noreply.github.com>
2021-11-27 12:05:48 +0530

    Add files via upload

---
+++ Vowelsconsosants.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+/* ###################################################*/
+/*                                                    */
+/*  AUTHOR:  VS NIKHIL MAMHESWAR                      */       
+/*                                                    */
+/*  PROBLEM: FIND THE VOWELS,CONCONSANTS IN A STRING  */
+/*                                                    */          
+/*  DATE   : 27-11-2021                               */
+/*                                                    */
+/*####################################################*/
+public class Vowelsconsosants {
+	public static void main(String agrs[]) 
+	{
+	Scanner s=new Scanner(System.in);
+	System.out.println("enter the sentence");
+	String str=s.nextLine();
+	int c=0;
+	int v=0;
+	for(int i=0;i<str.length();i++)
+	{
+		char ch=str.charAt(i);
+		if(ch=='a'|| ch=='A' || ch=='E' || ch=='e' || ch=='I' || ch=='i' || ch=='O' || ch=='o'|| ch=='u'|| ch=='U')
+		{
+			
+			v++;
+		}
+		else if(ch>='a' && ch<='z')
+		{
+			
+			c++;
+		}
+	}
+System.out.println("vowels = "+v);
+System.out.println("consonants = "+c);
+}
+}
